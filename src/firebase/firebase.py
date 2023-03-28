c19c15cbff8bb5d54b6c8f9e57c42aa4a9fe48bd
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2023-03-28 17:26:13 -0300

    configuracion inicial de firebase y base de datos en tiempo real

---
+++ src/firebase/firebase.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+#Credenciales de firebase
+firebase_sdk = credentials.Certificate('./sanva-project-54f54-firebase-adminsdk-km05b-b6e691f9f4.json')
+
+#Referencia a la base de datos
+firebase_admin.initialize_app(firebase_sdk,{'databaseURL':'https://sanva-project-54f54-default-rtdb.firebaseio.com/'})
+
+ref = db.reference('/gollds')
+ref.push({'user':'dasdasdwqeqwds','password':'Dani123','email':'daniel@gmail.com'})
