8f0fefb8f8656051a830bd94812c43b47a28a583
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2023-03-30 12:27:54 -0300

    remap firebase crud and added login functionalities

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-from schemas import User
-from firebase.post_user import run as run_post_user
-from firebase.get_user_by_email import get_user
+from schemas import User, Login
+from firebase.firebase_crud import get_user, find_user, post_user
+import firebase_admin
+from firebase_admin import credentials
+
+#Credenciales de firebase
+firebase_sdk = credentials.Certificate("./sanva-project-54f54-firebase-adminsdk-km05b-b6e691f9f4.json")
+
+#Referencia a la base de datos
+firebase_admin.initialize_app(firebase_sdk,{'databaseURL':'https://sanva-project-54f54-default-rtdb.firebaseio.com/'})
 
 app = FastAPI()
 
@@ -10,15 +17,16 @@ def index():
     return ("hola mundo")
 
 @app.post('/user')
-async def post_user(user : User):
+async def register_user(user : User):
     try:
         new_user = {
             "username" : user.user,
             "password" : user.password,
             "email" : user.email
         }
-        run_post_user(new_user)
-        return "usuario creado"
+        post_user(new_user)
+        if new_user:
+            return "User successfully registered"
     except Exception as e:
         return {"error": str(e)}
     
@@ -28,6 +36,13 @@ async def get_user_by_email(email : str):
         find_user = get_user(email)
         if find_user:
             return find_user
-        return "user not found"
+        return "User not found"
     except Exception as e:
         return {"error": str(e)}
+    
+@app.post("/login")
+async def log_in_user(user : Login):
+    try:
+        return find_user(user.username, user.password)
+    except Exception as e:
+        return {"error" : e}
