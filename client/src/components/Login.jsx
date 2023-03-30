cbb28363f29322d4e39c8013b2bea9ccab80da17
German Horianski <germanezequielhorianski@gmail.com>
2023-03-30 19:14:15 -0300

    Resizing elements of Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import axios from 'axios'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import Persona from '../assets/logo_persona_azul_2.jpg'
+import Logo from '../assets/logo_sanva.jpg'
 import { useNavigate } from 'react-router'
 
 export const Login = () => {
   const navigate = useNavigate()
   const Logearse = async (values) => {
     try {
-      const response = await axios.post('http://localhost:8000/login', values)
+      const response = await axios.post('https://purebadeploy.onrender.com/login', values)
       console.log(response.data)
       if (response.data === 'Succesfully logged in') {
         navigate('/user')
@@ -54,8 +55,9 @@ export const Login = () => {
         <div className="absolute w-screen h-screen skew-y-[40deg] bg-[#8dcfec]"></div>
         <div className="absolute w-screen h-screen translate-y-10 skew-y-[40deg] bg-[#6abce2]"></div>
         <div className="absolute w-screen h-screen translate-y-20 skew-y-[40deg] bg-[#58afdd]"></div>
-        <div className="absolute w-screen h-screen translate-y-[35rem] skew-y-[40deg] bg-[#3982b8]"></div>
-        <div className="absolute flex flex-wrap justify-center translate-y-[50px]">
+        <div className="absolute w-screen h-screen translate-y-[48rem] skew-y-[40deg] bg-[#3982b8]"></div>
+        <div className="absolute flex flex-wrap justify-center translate-y-[160px]">
+        <img src={Logo} alt="logo_sanva" className="w-[10rem] h-[10rem]"></img>
 
           <Formik initialValues={{ username: '', password: '' }} onSubmit={HandleSubmit
           } validate={ValidateFields}>
@@ -64,7 +66,7 @@ export const Login = () => {
               <Form className="w-3/5 flex flex-wrap space-y-4">
                 <Field name="username" type="username" placeholder="Nombre" className="w-[200px] bg-white rounded-[5px] text-black"/>
               <ErrorMessage name="username" />
-                <Field name="password" type="password" placeholder="ContraseÃ±a" className="w-[200px] bg-white rounded-[5px] text-black"/>
+                <Field name="password" type="password" placeholder="Contraseña" className="w-[200px] bg-white rounded-[5px] text-black"/>
               <ErrorMessage name="password" type="password" />
               <button disabled={isSubmitting} type="submit" className="w-[200px] h-[1.5rem] leading-[0.1rem] rounded-[5px] bg-[#3271a5]">Log In</button>
               </Form>
