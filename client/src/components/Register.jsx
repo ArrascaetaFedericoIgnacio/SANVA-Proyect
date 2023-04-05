f8edd55ed9dd30865648762e2b7b14f48f1c7b78
German Horianski <germanezequielhorianski@gmail.com>
2023-04-04 23:10:26 -0300

    Add setItem for username and password (temporarily)

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,101 +1,98 @@
-	import React from "react";
-	import { Formik, Form, Field, ErrorMessage } from "formik";
-	import { HiOutlineUser, HiOutlineLockClosed, HiOutlineMail } from "react-icons/hi"
-	import { CheckIcon } from "./check";
-	import Check from "../../public/checked.svg"
-	import Logo from "../../public/logosanva.png"
-	import axios from 'axios';
-	import { useNavigate } from "react-router-dom";
-	import emailjs from '@emailjs/browser';
-	
-	
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { HiOutlineUser, HiOutlineLockClosed, HiOutlineMail } from 'react-icons/hi'
+import { CheckIcon } from './check'
+import Check from '../../public/checked.svg'
+import Logo from '../../public/logosanva.png'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import emailjs from '@emailjs/browser'
 
+export const Register = () => {
+  const navigate = useNavigate()
 
-	export const Register = () => {
-		const navigate = useNavigate()
-		
+  const CrearCodigo = () => {
+    const codigo = Math.floor(Math.random() * 1000000) // crear un código de verificación aleatorio
+    localStorage.setItem('codigo', codigo) // guardar el código en el almacenamiento local
+  }
 
-		const CrearCodigo = () => {
-			const codigo = Math.floor(Math.random() * 1000000); // crear un código de verificación aleatorio
-			localStorage.setItem("codigo", codigo); // guardar el código en el almacenamiento local
-		}
+  const EnviarCodigo = (email) => {
+    const codigo = localStorage.getItem('codigo') // obtener el código de verificación del almacenamiento local
+    const templateParams = {
+      to_name: email,
+      message: `Tu código de verificación es: ${codigo}`,
+      from_name: 'Sanva'
+    }
+    emailjs.send(
+      'service_1nuri73',
+      'template_5kznyer',
+      templateParams,
+      'v5ygCVGVTrm0Eyvxw'
+    )
+  }
 
-		const EnviarCodigo = (email) => {
-			const codigo = localStorage.getItem("codigo"); // obtener el código de verificación del almacenamiento local
-			const templateParams = {
-				to_name: email,
-				message: `Tu código de verificación es: ${codigo}`,
-				from_name: "Sanva",
-			};
-			emailjs.send(
-				"service_1nuri73",
-				"template_5kznyer",
-				templateParams,
-				"v5ygCVGVTrm0Eyvxw"
-			);
-		};
+  const HandleSubmit = (values, { setSubmitting }) => {
+    setSubmitting(false)
+    console.log(values)
+    const email = values.email // obtener el valor del campo de entrada de correo electrónico
+    // guarda en localstorage el email
+    localStorage.setItem('username', values.username)
+    localStorage.setItem('password', values.password)
+    localStorage.setItem('email', email)
+    CrearCodigo() // crear un nuevo código de verificación
+    // CrearCuenta(values);
+    EnviarCodigo(email) // enviar el código de verificación por correo electrónico
 
-	const HandleSubmit = (values, { setSubmitting }) => {
-		setSubmitting(false);
-		console.log(values);
-		const email = values.email; // obtener el valor del campo de entrada de correo electrónico
-		//guarda en localstorage el email
-		localStorage.setItem("email", email);
-		CrearCodigo(); // crear un nuevo código de verificación
-		// CrearCuenta(values);
-		EnviarCodigo(email); // enviar el código de verificación por correo electrónico
+    navigate('/verification')
+  }
 
-		navigate('/verification');
-	};
-		
+  // const CrearCuenta = async (values) => {
+  // 	try {
+  // 	console.log(values);
+  // 	const resultado = await axios.post(
+  // 		'https://purebadeploy.onrender.com/user',
+  // 		values
+  // 	);
+  // 	EnviarCodigo(values.email); // enviar el código de verificación por correo electrónico
+  // 	console.log(resultado);
+  // 	navigate('/user');
+  // 	} catch (error) {
+  // 	console.log(error);
+  // 	}
+  // };
 
-		// const CrearCuenta = async (values) => {
-	// 	try {
-	// 	console.log(values);
-	// 	const resultado = await axios.post(
-	// 		'https://purebadeploy.onrender.com/user',
-	// 		values
-	// 	);
-	// 	EnviarCodigo(values.email); // enviar el código de verificación por correo electrónico
-	// 	console.log(resultado);
-	// 	navigate('/user');
-	// 	} catch (error) {
-	// 	console.log(error);
-	// 	}
-	// };
+  const validateCamps = (values) => {
+    const errors = {}
 
-		const validateCamps = (values) => {
-			let errors = {};
+    // validate name
+    if (!values.username) {
+      errors.username = 'El nombre es obligatorio'
+    }
+    // verify password
+    if (!values.password) {
+      errors.password = 'El password es obligatorio'
+    } else if (values.password.length < 6) {
+      errors.password = 'El password debe tener al menos 6 caracteres'
+    }
+    // check if passwords are the same
+    // if (values.verifyPassword !== values.password) {
+    // 	errors.verifyPassword = 'Las contraseñas no coinciden';
+    // }
+    // check email
+    if (!values.email) {
+      errors.email = 'El correo es requerido'
+    } else if (
+      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+        values.email
+      )
+    ) {
+      errors.email = 'Email invalido'
+    }
+    return errors
+  }
 
-			// validate name
-			if (!values.username) {
-				errors.username = 'El nombre es obligatorio';
-			}
-			// verify password
-			if (!values.password) {
-				errors.password = 'El password es obligatorio';
-			} else if (values.password.length < 6) {
-				errors.password = 'El password debe tener al menos 6 caracteres';
-			}
-			// check if passwords are the same
-			// if (values.verifyPassword !== values.password) {
-			// 	errors.verifyPassword = 'Las contraseñas no coinciden';
-			// }
-			//check email
-			if (!values.email) {
-				errors.email = 'El correo es requerido';
-			} else if (
-				!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
-					values.email
-				)
-			) {
-				errors.email = 'Email invalido';
-			}
-			return errors;
-		};
-
-		return (
-			<div className="relative w-screen h-screen flex justify-center items-center">
+  return (
+		<div className="relative w-screen h-screen flex justify-center items-center">
 			<div className="absolute w-screen h-screen -translate-y-40 skew-y-[40deg] bg-[#6abce2]"></div>
 			<div className="absolute w-screen h-screen translate-y-20 skew-y-[40deg] bg-[#58afdd]"></div>
 			<div className="absolute w-screen h-screen translate-y-[48rem] skew-y-[40deg] bg-[#3982b8]"></div>
@@ -104,10 +101,10 @@
 				<img className='mt-10 rounded-full w-32' src="https://us.123rf.com/450wm/imagevectors/imagevectors1606/imagevectors160600225/58872992-blanco-perfil-de-usuario-icono-en-el-bot%C3%B3n-azul-aislado-en-blanco.jpg" alt="user" />
 				<Formik
 					initialValues={{
-						username: '',
-						password: '',
-						email: ''
-						// verifyPassword: '',
+					  username: '',
+					  password: '',
+					  email: ''
+					  // verifyPassword: '',
 					}}
 					onSubmit={HandleSubmit}
 					validate={validateCamps}>
@@ -147,7 +144,7 @@
 									</div>
 								)}
 							</Field>
-					
+
 							<Field name='email'>
 								{({ field, form: { touched, errors }, meta }) => (
 									<div>
@@ -165,7 +162,7 @@
 									</div>
 								)}
 							</Field>
-					
+
 							<div className="mt-5">
 								<button className="outline-none rounded-full " disabled={isSubmitting} type="submit">
 									<img className="hover:border-sky-700 hover:border-2 hover:rounded-full" src={Check}></img>
@@ -175,6 +172,6 @@
 					)}
 				</Formik>
 			</div>
-			</div>
-		);
-	};
+		</div>
+  )
+}
