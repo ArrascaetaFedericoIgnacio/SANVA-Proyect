aca34577297e8ff37bf5fd684f03e1a0f680e84c
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-05 18:15:40 -0300

    asd

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,55 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import { FaPencilAlt } from 'react-icons/fa'
-import { Footer } from './footer'
+import React, { useEffect, useState } from 'react';
+import { FaPencilAlt } from 'react-icons/fa';
+import { Footer } from './footer';
 
 export const User = () => {
-  const [user, setUserLogin] = useState(null)
-  console.log(user)
-  
-  useEffect(() => {
-    const userlocal = localStorage.getItem('user')
-    setUserLogin(userlocal)
-    console.log(userlocal)
-  },[])
-  const { username, email, birthdate, gender, height, weight } = user
+  const [user, setUser] = useState(null);
+  console.log(user);
 
-  
-  
+  useEffect(() => {
+    const userlocal = localStorage.getItem('user');
+    setUser(JSON.parse(userlocal));
+    console.log(userlocal);
+  }, []);
+
+  const { username, email, birthdate, gender, height, weight } = user || {};
+
   return (
-    <div className='h-full w-full text-lg font-semibold'>
-      <div className='py-8 flex flex-col justify-between items-center bg-[#6abce2] text-white'>
-        <img className='mt-3 rounded-full w-28' src="https://us.123rf.com/450wm/imagevectors/imagevectors1606/imagevectors160600225/58872992-blanco-perfil-de-usuario-icono-en-el-bot%C3%B3n-azul-aislado-en-blanco.jpg" alt="user" />
-        <div className='flex items-center gap-2'>
-         {username ? <h3 className='py-3'>{username}</h3> : <h3 className='py-3'>Nombre</h3>} 
-          <FaPencilAlt/>
+    <div className="h-full w-full text-lg font-semibold">
+      <div className="py-8 flex flex-col justify-between items-center bg-[#6abce2] text-white">
+        <img
+          className="mt-3 rounded-full w-28"
+          src="https://us.123rf.com/450wm/imagevectors/imagevectors1606/imagevectors160600225/58872992-blanco-perfil-de-usuario-icono-en-el-bot%C3%B3n-azul-aislado-en-blanco.jpg"
+          alt="user"
+        />
+        <div className="flex items-center gap-2">
+          {username ? (
+            <h3 className="py-3">{username}</h3>
+          ) : (
+            <h3 className="py-3">Nombre</h3>
+          )}
+          <FaPencilAlt />
         </div>
         <p>{email}</p>
       </div>
-      <div className='flex flex-col py-8 gap-3 px-14 bg-[#58afdd] text-white'>
-        <div className='flex justify-center items-center gap-2'>
+      <div className="flex flex-col py-8 gap-3 px-14 bg-[#58afdd] text-white">
+        <div className="flex justify-center items-center gap-2">
           <p>Informacion general</p>
           <FaPencilAlt />
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <p>Fecha</p>
           <p>{birthdate}</p>
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <p>Sexo</p>
-          <p>{gender === "m" ? "Masculino" : "Femenino"}.</p>
+          <p>{gender === 'm' ? 'Masculino' : 'Femenino'}.</p>
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <p>Talla</p>
           <p>{height} Mts.</p>
         </div>
-        <div className='flex justify-between'>
+        <div className="flex justify-between">
           <p>Peso</p>
           <p>{weight} Kg.</p>
         </div>
       </div>
-      <div className='px-4 pt-8 pb-24 bg-[#3982b8] text-white'>
-        <p className='mb-3'>Informacion Medica</p>
-        <div className='flex flex-col items-start px-10 gap-3'>
+      <div className="px-4 pt-8 pb-24 bg-[#3982b8] text-white">
+        <p className="mb-3">Informacion Medica</p>
+        <div className="flex flex-col items-start px-10 gap-3">
           <p>Enfermedades</p>
           <p>Medicamento</p>
           <p>Alergias</p>
@@ -58,5 +65,5 @@ export const User = () => {
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
