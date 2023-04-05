7d3a446d593f1acf10e1ea57ebc7d68f244af6dc
German Horianski <germanezequielhorianski@gmail.com>
2023-04-05 16:09:33 -0300

    Correct line 21

--- client/src/components/PersonalData.jsx
+++ client/src/components/PersonalData.jsx
@@ -19,7 +19,7 @@ export const PersonalData = () => {
         values
       )
       console.log(response)
-      localStorage.setItem('user', response)
+      localStorage.setItem('user', response.data)
       navigate('/user')
     } catch (error) {
       console.log(error)
