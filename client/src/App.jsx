a17837c4efb3fe1467f211e45ee6a24fbacd46a6
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2023-04-04 21:55:22 -0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,26 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Register } from './components/Register'
-import { Login } from './components/Login'
-import { User } from './components/User'
-import { Footer } from './components/footer'
-import { Verification } from './components/Verification'
-import Tomas from './components/Tomas'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Register } from './components/Register';
+import { Login } from './components/Login';
+import { User } from './components/User';
+import { Footer } from './components/footer';
+import { Verification } from './components/Verification';
+import Tomas from './components/Tomas';
 
-
-function App () {
-  return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Login />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/verification" element={<Verification />} />
-          <Route path='/tomas' element={<Tomas />} />
-        </Routes>
-
-      </div>
-    </BrowserRouter>
-  )
+function App() {
+	return (
+		<BrowserRouter>
+			<div>
+				<Routes>
+					<Route path='/register' element={<Register />} />
+					<Route path='/' element={<Login />} />
+					<Route path='/user' element={<User />} />
+					<Route path='/verification' element={<Verification />} />
+					<Route path='/tomas' element={<Tomas />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
