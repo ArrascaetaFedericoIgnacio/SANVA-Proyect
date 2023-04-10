f3ef8b961e90f6df8523e3241f1ee3197fc04f19
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-10 11:05:02 -0300

    asd

--- client/src/components/DiseasesList.jsx
+++ client/src/components/DiseasesList.jsx
@@ -1,14 +1,55 @@
-
+import React, { useState } from "react";
+import { Footer } from "./footer";
+import { HiOutlineHeart, HiCalendar } from "react-icons/hi";
+import { BsBuildings } from "react-icons/bs";
+import AccordionData from "./AccodionData";
 
 const Diseases = () => {
+  const accordionData = [
+    {
+      title: "04 de Abril de 2023",
+      description: "lorem",
+    },
+    {
+      title: "03/04/2023",
+      description: "lorem",
+    },
+    {
+      title: "02/04/2023",
+      description: "lorem",
+    },
+    {
+      title: "01/04/2023",
+      description: "lorem",
+    },
+    {
+      title: "31/03/2023",
+      description: "lorem",
+    },
+    {
+      title: "30/03/2023",
+      description: "lorem",
+    },
+    {
+      title: "29/03/2023",
+      description: "lorem",
+    },
+  ];
+
+  const [open, setOpen] = useState(false);
+
+  const toggle = (index) => {
+    if (open === index) {
+      return setOpen(null);
+    }
+    setOpen(index);
+  };
+
   return (
     <div className="max-w-360 mx-auto h-full">
       <h2 className="text-center text-2xl font-bold mt-20">Enfermedades</h2>
       <div>
-        <p className="text-center text-sm mt-5">
-            Enfermedades
-        </p>
-
+        <p className="text-center text-sm mt-5">Enfermedades</p>
       </div>
       <div className="flex justify-between mt-20 mb-10 bg-[#6abce2]">
         <button className="bg-blue-500 text-white px-4 py-2 rounded-lg font-bold border-none">
@@ -18,10 +59,11 @@ const Diseases = () => {
           Agregar Enfermedad
         </button>
       </div>
-   
-        <div className="flex justify-between items-center mt-5">
-            <p className="font-bold text-lg my-0 w-3/5">Agregar</p>
-        </div>
+
+      <div className="flex justify-between items-center mt-5">
+        <p className="font-bold text-lg my-0 w-3/5">Agregar</p>
+      </div>
+      
       <div className="bg-gray-200 p-4 rounded-lg">
         <div className="flex justify-between items-center">
           <p className="font-bold text-lg my-0 w-3/5">Diabetes</p>
@@ -41,7 +83,9 @@ const Diseases = () => {
         </div>
       </div>
     </div>
-  );
+
+    
+    );
 };
 
 export default Diseases;
