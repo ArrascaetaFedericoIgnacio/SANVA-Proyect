4b77858a3167fe76a97c9f53afbfda859b87f53d
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-10 11:19:10 -0300

    asd

--- client/src/components/DiseasesList.jsx
+++ client/src/components/DiseasesList.jsx
@@ -5,50 +5,10 @@ import { BsBuildings } from "react-icons/bs";
 import AccordionData from "./AccodionData";
 
 const Diseases = () => {
-  const accordionData = [
-    {
-      title: "04 de Abril de 2023",
-      description: "lorem",
-    },
-    {
-      title: "03/04/2023",
-      description: "lorem",
-    },
-    {
-      title: "02/04/2023",
-      description: "lorem",
-    },
-    {
-      title: "01/04/2023",
-      description: "lorem",
-    },
-    {
-      title: "31/03/2023",
-      description: "lorem",
-    },
-    {
-      title: "30/03/2023",
-      description: "lorem",
-    },
-    {
-      title: "29/03/2023",
-      description: "lorem",
-    },
-  ];
-
-  const [open, setOpen] = useState(false);
-
-  const toggle = (index) => {
-    if (open === index) {
-      return setOpen(null);
-    }
-    setOpen(index);
-  };
-
   return (
     <div className="max-w-360 mx-auto h-full bg-[#6abce2]">
-      <h2 className="text-center text-2xl font-bold mt-20">Enfermedades</h2>
-      <div>
+      <h2 className="text-center text-2xl font-bold mt-20  bg-[#6abce2]">Enfermedades</h2>
+      <div className=" bg-[#6abce2]">
         <p className="text-center text-sm mt-5">Enfermedades</p>
       </div>
       <div className="flex justify-between mt-20 px-5 mb-10 bg-[#6abce2]">
@@ -66,20 +26,20 @@ const Diseases = () => {
       
       <div className="bg-gray-200 p-4 rounded-lg">
         <div className="flex justify-between items-center">
-          <p className="font-bold text-lg my-0 w-3/5">Diabetes</p>
-          <p className="text-right text-sm my-0 w-2/5">Dr. Juan Pérez</p>
+          <p className="font-bold text-lg my-0 w-3/5 text-black">Diabetes</p>
+          <p className="text-right text-sm my-0 w-2/5 text-black">Dr. Juan Pérez</p>
         </div>
         <div className="flex justify-between items-center mt-5">
-          <p className="font-bold text-lg my-0 w-3/5">Hipertensión</p>
-          <p className="text-right text-sm my-0 w-2/5">Dr. Marta García</p>
+          <p className="font-bold text-lg my-0 w-3/5 text-black">Hipertensión</p>
+          <p className="text-right text-sm my-0 w-2/5 text-black">Dr. Marta García</p>
         </div>
         <div className="flex justify-between items-center mt-5">
-          <p className="font-bold text-lg my-0 w-3/5">Asma</p>
-          <p className="text-right text-sm my-0 w-2/5">Dr. Ana Flores</p>
+          <p className="font-bold text-lg my-0 w-3/5 text-black">Asma</p>
+          <p className="text-right text-sm my-0 w-2/5 text-black">Dr. Ana Flores</p>
         </div>
         <div className="flex justify-between items-center mt-5">
-          <p className="font-bold text-lg my-0 w-3/5">Artritis</p>
-          <p className="text-right text-sm my-0 w-2/5">Dr. Luis González</p>
+          <p className="font-bold text-lg my-0 w-3/5 text-black">Artritis</p>
+          <p className="text-right text-sm my-0 w-2/5 text-black">Dr. Luis González</p>
         </div>
       </div>
     </div>
