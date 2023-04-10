0b3a15ac3802d86c498b919c9ab5aa9760e70ec9
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-10 10:09:55 -0300

    asd

---
+++ client/src/components/DiseasesList.jsx
@@ -0,0 +1,37 @@
+
+
+const Diseases = () => {
+  return (
+    <div className="max-w-360 mx-auto">
+      <h2 className="text-center text-2xl font-bold mt-20">Enfermedades</h2>
+      <div className="flex justify-between mt-20 mb-10">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg font-bold border-none">
+          Ver Enfermedades
+        </button>
+        <button className="bg-red-500 text-white px-4 py-2 rounded-lg font-bold border-none">
+          Agregar Enfermedad
+        </button>
+      </div>
+      <div className="bg-gray-200 p-4 rounded-lg">
+        <div className="flex justify-between items-center">
+          <p className="font-bold text-lg my-0 w-3/5">Diabetes</p>
+          <p className="text-right text-sm my-0 w-2/5">Dr. Juan Pérez</p>
+        </div>
+        <div className="flex justify-between items-center mt-5">
+          <p className="font-bold text-lg my-0 w-3/5">Hipertensión</p>
+          <p className="text-right text-sm my-0 w-2/5">Dr. Marta García</p>
+        </div>
+        <div className="flex justify-between items-center mt-5">
+          <p className="font-bold text-lg my-0 w-3/5">Asma</p>
+          <p className="text-right text-sm my-0 w-2/5">Dr. Ana Flores</p>
+        </div>
+        <div className="flex justify-between items-center mt-5">
+          <p className="font-bold text-lg my-0 w-3/5">Artritis</p>
+          <p className="text-right text-sm my-0 w-2/5">Dr. Luis González</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Diseases;
--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { FaPencilAlt } from 'react-icons/fa'
 import { Footer } from './footer'
+import { NavLink } from 'react-router-dom'
 
 export const User = () => {
   const [user, setUser] = useState(null)
@@ -62,7 +63,9 @@ export const User = () => {
       <div className="px-4 pt-8 pb-24 bg-[#3982b8] text-white">
         <p className="mb-3">Informacion Medica</p>
         <div className="flex flex-col items-start px-10 gap-3">
-          <p>Enfermedades</p>
+           <NavLink to="/diseases" className="text-black">
+            <p>Enfermedades</p>
+          </NavLink>
           <p>Medicamento</p>
           <p>Alergias</p>
           <p>Insumos o Servicios</p>
