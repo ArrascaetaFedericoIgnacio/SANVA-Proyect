070773eff742af92d15e468d7a13bc78658f4462
German Horianski <germanezequielhorianski@gmail.com>
2023-04-10 21:24:31 -0300

    Create DugsForm

---
+++ client/src/components/DrugsForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Footer } from './footer.jsx'
+
+const DrugsForm = () => {
+  return (
+    <div>
+        <header className="w-screen h-52 bg-[#4194cb] text-white">
+            <h1>Medicamentos</h1>
+            <img></img>
+            <img></img>
+        </header>
+        <main>
+            <h3>Agregar Medicamento</h3>
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default DrugsForm
