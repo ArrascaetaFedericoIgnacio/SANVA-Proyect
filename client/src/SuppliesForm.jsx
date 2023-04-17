d4a1017f495a89740828d61d64647e49d4fbfdfa
German Horianski <germanezequielhorianski@gmail.com>
2023-04-17 18:08:09 -0300

    Add react icons

--- client/src/SuppliesForm.jsx
+++ client/src/SuppliesForm.jsx
@@ -4,12 +4,13 @@ import { Link } from 'react-router-dom'
 import { Formik, Form, Field } from 'formik'
 import { useNavigate } from 'react-router'
 import { BiPlusMedical } from 'react-icons/bi'
-import { BsCircle, BsVirus } from 'react-icons/bs'
+import { BsBriefcaseFill } from 'react-icons/bs'
+import { FaBriefcaseMedical } from 'react-icons/fa'
 import { checkSwitch } from './switch.js'
 import axios from 'axios'
 
 const DrugsForm = () => {
-//   const navigate = useNavigate()
+  const navigate = useNavigate()
 
   //   const PostInfo = async (values) => {
   //     try {
@@ -23,11 +24,11 @@ const DrugsForm = () => {
   //     }
   //   }
 
-  //   const HandleSubmit = (values, { setSubmitting }) => {
-  //     setSubmitting(false)
-  //     PostInfo(values)
-  //     navigate('/user')
-  //   }
+  const HandleSubmit = (values, { setSubmitting }) => {
+    setSubmitting(false)
+    PostInfo(values)
+    navigate('/user')
+  }
 
   //   const validateFields = (values) => {
   //     const errors = {}
@@ -55,18 +56,18 @@ const DrugsForm = () => {
   return (
     <div>
         <header className="w-screen h-52 bg-[#4194cb] text-white flex justify-center items-center flex-col">
-            <h1>Alergias</h1>
+            <h1>Insumos</h1>
             <div className="flex flex-row space-x-7 mt-12">
-            <Link to="/allergieslist">
+            <Link to="/serviceslist">
                 <div>
                 <BsCircle className="w-11 h-11"/>
-                <BsVirus className="w-8 h-8 absolute -translate-y-[38px] translate-x-[6px]"/>
+                <BsBriefcaseFill className="w-8 h-8 absolute -translate-y-[38px] translate-x-[6px]"/>
                 </div>
             </Link>
             {/* <Link to="/AllergiesForm"> */}
                 <div>
                 <BsCircle className="w-11 h-11"/>
-                <BsVirus className="w-8 h-8 absolute -translate-y-[38px] translate-x-[6px]"/>
+                <FaBriefcaseMedical className="w-8 h-8 absolute -translate-y-[38px] translate-x-[6px]"/>
                 <BiPlusMedical className="w-3 h-3 absolute text-black -translate-y-6 translate-x-5"/>
                 </div>
             {/* </Link> */}
@@ -117,7 +118,7 @@ const DrugsForm = () => {
                     {({ field, form: { touched, errors }, meta }) => (
                         <div className="border-b-[1.8px] border-[#3982b8]">
                         <div className="w-screen bg-white text-slate-400 py-[17.5px] flex justify-between px-[30px] items-center">
-                        <label className="w-[50%]">Vacuna </label>
+                        <label className="w-[50%]">Tipo </label>
                         <input type="text" className="bg-white w-[50%] text-center rounded-lg outline-none border-2 focus:border-sky-600 transition duration-200" placeholder="Nombre de vacuna" {...field}></input>
                         {meta.touched && meta.error && (
                             <div className='pt-2 text-red-600 font-semibold'>
@@ -153,7 +154,7 @@ const DrugsForm = () => {
                     {({ field, form: { touched, errors }, meta }) => (
                         <div className="border-b-[1.8px] border-[#3982b8]">
                         <div className="w-screen bg-white text-slate-400 py-[17.5px] flex justify-between items-center">
-                        <label className="w-[60%]">Frecuencia de vacuna </label>
+                        <label className="w-[60%]">Días de consumo </label>
                         <select name="vaccineFrequency" className="w-[40%] mr-10 bg-white text-center rounded-lg outline-none border-2 focus:border-sky-600 transition duration-200" {...field}>
                             <option value="oncePerWeek">Semanal</option>
                             <option value="oncePerThreeWeeks">Cada tres semanas</option>
@@ -176,7 +177,7 @@ const DrugsForm = () => {
                     {({ field, form: { touched, errors }, meta }) => (
                         <div className="border-b-[1.8px] border-[#3982b8]">
                         <div className="w-screen bg-white text-slate-400 py-[16px] flex justify-between px-[30px] items-center">
-                        <label className="w-[70%]">Recordatorio de vacuna: </label>
+                        <label className="w-[70%]">Proveedor </label>
                                 <input
                                 className={checkSwitch}
                                 type='checkbox'
@@ -224,7 +225,7 @@ const DrugsForm = () => {
                         {({ field, form: { touched, errors }, meta }) => (
                         <div>
                             <div className="py-4 flex justify-between px-[30px] items-center">
-                                {/* <p>Comentarios</p> */}
+                                <p>Alerta por falta de inventario </p>
                                 <textarea
                                 className='text-black h-20 w-full rounded-lg bg-white px-4 py-3 outline-none border-2 focus:border-sky-600 transition duration-200'
                                 type='textarea'
