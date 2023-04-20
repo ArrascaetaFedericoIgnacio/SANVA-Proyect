d2753ff367b57a94c5ddb87651d70968d42f95e8
_ArrascaetaFedericoIgnacio_ <_arrascaetaefdev@gmail.com_>
2023-04-20 00:30:55 -0300

    asd

--- client/src/store/useAllergies.jsx
+++ client/src/store/useAllergies.jsx
@@ -1,40 +1,40 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 
 const useAllergies = create((set) => ({
-    allergies: [ 
-            {
-              nombre: "nombre",
-              vacuna: "Vacuna",
-              inventario: "Inventario",
-              nextVacuna: "Proxima vacuna",
-            },
-            {
-              nombre: "nombre",
-              vacuna: "Vacuna",
-              inventario: "Inventario",
-              nextVacuna: "Proxima vacuna",
-            },
-            {
-              nombre: "nombre",
-              vacuna: "Vacuna",
-              inventario: "Inventario",
-              nextVacuna: "Proxima vacuna",
-            },
-            {
-              nombre: "nombre",
-              vacuna: "Vacuna",
-              inventario: "Inventario",
-              nextVacuna: "Proxima vacuna",
-            },
-            {
-              nombre: "nombre",
-              vacuna: "Vacuna",
-              inventario: "Inventario",
-              nextVacuna: "Proxima vacuna",
-            },
-    ],
-    setallergies: (allergies) => set({...allergies,allergies}),
-    clearallergies: () => set({allergies: {}}),
+  allergies: [
+    {
+      nombre: "nombre",
+      vacuna: "Vacuna",
+      inventario: "Inventario",
+      nextVacuna: "Proxima vacuna",
+    },
+    {
+      nombre: "nombre",
+      vacuna: "Vacuna",
+      inventario: "Inventario",
+      nextVacuna: "Proxima vacuna",
+    },
+    {
+      nombre: "nombre",
+      vacuna: "Vacuna",
+      inventario: "Inventario",
+      nextVacuna: "Proxima vacuna",
+    },
+    {
+      nombre: "nombre",
+      vacuna: "Vacuna",
+      inventario: "Inventario",
+      nextVacuna: "Proxima vacuna",
+    },
+    {
+      nombre: "nombre",
+      vacuna: "Vacuna",
+      inventario: "Inventario",
+      nextVacuna: "Proxima vacuna",
+    },
+  ],
+  setAllergies: (allergies) => set({ allergies }),
+  clearAllergies: () => set({ allergies: [] }),
 }))
 
 export default useAllergies
